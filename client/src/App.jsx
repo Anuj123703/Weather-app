cb6be59e9404490a8304a7bd2b6000cf6f9b7c25
Anuj Kumar <2023041120@mmmut.ac.in>
2026-03-31 22:16:10 +0530

    fix navbar path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./layouts/navbar";
+import Navbar from "./layouts/Navbar";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
